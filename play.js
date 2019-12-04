8d97f63b8b24437348c94d42641cb0745ad12011
Don-Stevenson <dsteven1@gmail.com>
2019-12-04 20:37:58 +0000

    refactored to input.js

--- play.js
+++ play.js
@@ -1,31 +1,9 @@
-// const net = require('net');
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
 
-
-handleUserInput = key => {
-  if (key === '\u0003') {
-    console.log("exiting the game now");
-    process.exit();
-  }
-};
-
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- * 
- * 
- * 
- */
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on("data", handleUserInput)
-  return stdin;
-}
-
 setupInput();
+
 
