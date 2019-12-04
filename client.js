fba77b8b0b4cb1fdccd02388aedb29e875c50575
Don-Stevenson <dsteven1@gmail.com>
2019-12-04 19:34:18 +0000

    updated play to handle user input

--- client.js
+++ client.js
@@ -3,22 +3,28 @@ const net = require('net');
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
-  const conn = net.createConnection({ 
+const connect = function () {
+  const conn = net.createConnection({
     host: '172.46.2.204',
     port: 50541
   });
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-
+  conn.setEncoding('utf8');
+  // on connect, sends the message and writes to the server that I am DBS
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
     conn.write('Name: DBS');
-   });
+    conn.write("Move: up");
+    conn.write("Move: down");
+    conn.write("Move: left");
+    conn.write("Move: right");
+  });
+
+  conn.on('data', (data) => {
+    console.log(data);
+ 
+  });
+
 
   return conn;
 }
--- play.js
+++ play.js
@@ -3,3 +3,29 @@ const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
 
+
+handleUserInput = key => {
+  if (key === '\u0003') {
+    console.log("exiting the game now");
+    process.exit();
+  }
+};
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ * 
+ * 
+ * 
+ */
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on("data", handleUserInput)
+  return stdin;
+}
+
+setupInput();
+
