8507177238ce62550fbbd09dc03f14bdc3b119b5
Don-Stevenson <dsteven1@gmail.com>
2019-12-04 21:10:57 +0000

    completed and working version!

--- input.js
+++ input.js
@@ -1,7 +1,34 @@
+// Stores the active TCP connection object.
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on("data", key => handleUserInput(key))
+  return stdin;
+}
+
+
+
 handleUserInput = key => {
   if (key === '\u0003') {
     console.log("exiting the game now");
     process.exit();
+  } else if (key === "w") {
+    connection.write("Move: up");
+  } else if (key === "s") {
+    connection.write("Move: down");
+  } else if (key === "d") {
+    connection.write("Move: right");
+  } else if (key === "a") {
+    connection.write("Move: left");
+  } else if (key === "g") {
+    connection.write("Say: OMG!!");
+  } else if (key === "l") {
+    connection.write("Say: LOL!!");
   }
 };
 
@@ -12,13 +39,5 @@ handleUserInput = key => {
  * 
  * 
  */
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on("data", handleUserInput)
-  return stdin;
-}
 
-module.exports = {setupInput};
+module.exports = { setupInput };
